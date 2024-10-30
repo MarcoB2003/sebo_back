5a1dc9b7acebe2dadde199754253efd8471388cf
Marco <124935811+MarcoB2003@users.noreply.github.com>
2024-10-30 11:05:49 -0300

    Add files via upload

---
+++ src/service/filme/alterarFilmeService.js
@@ -0,0 +1,11 @@
+import { alterarFilme } from "../../repository/filmeRepository.js";
+import { validarCamposObrigatoriosFilme } from "../../validation/filme/filmeValidation.js";
+
+export default async function alterarFilmeService(filmeObj, id){
+  //validar campos
+  validarCamposObrigatoriosFilme(filmeObj);    
+  
+  let linhasAfetadas = await alterarFilme(filmeObj, id);
+      if (linhasAfetadas == 0)
+        throw new Error('Nenhum filme alterado.');
+}
