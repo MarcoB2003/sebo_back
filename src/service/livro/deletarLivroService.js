a610a4feb152896b3325d5122ac098626754552d
Marco <124935811+MarcoB2003@users.noreply.github.com>
2024-11-08 09:11:36 -0300

    Delete src directory

--- src/service/livro/deletarLivroService.js
+++
@@ -1,7 +0,0 @@
-import { deletarLivro } from "../../repository/livroRepository.js";
-
-export default async function deletarLivroService(id) {
-    let linhasAfetadas = await deletarLivro(id);
-    if (linhasAfetadas === 0)
-        throw new Error('Nenhum livro foi removido.');
-}
--- src/validation/usuario/usuarioValidation.js
+++
@@ -1,8 +0,0 @@
-export function validarCredenciais(credenciais) {
-    if (!credenciais.email || !credenciais.email.includes('@')) {
-        throw new Error('Email inválido.');
-    }
-    if (!credenciais.senha || credenciais.senha.length < 6) {
-        throw new Error('Senha deve conter pelo menos 6 caracteres.');
-    }
-}
