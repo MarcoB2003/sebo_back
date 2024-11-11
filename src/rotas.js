136f348c9556af1837d57e2f1aab7a4296e2a747
Marco <124935811+MarcoB2003@users.noreply.github.com>
2024-11-10 21:52:31 -0300

    Update rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import livroController from './controller/livroController.js';
 import vendaController from './controller/vendaController.js'; // Importando o controller de vendas
+import ususarioController from './controller/usuarioController.js'
 
 export default function adicionarRotas(servidor) {
     servidor.use(livroController);
     servidor.use(vendaController); // Adicionando rotas de venda
+    servidor.use(ususarioController);
     servidor.use('/storage/capaLivro', express.static('./storage/capaLivro'));
 }
